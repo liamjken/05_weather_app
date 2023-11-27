ae4d1c82d13932b192e16c2ce99b2e26cc4e34fb
liamjken <liamjken@gmail.com>
2023-11-27 15:45:39 -0500

    first commit

---
+++ scripts/app.js
@@ -0,0 +1,71 @@
+const cityForm = document.querySelector('form');
+const card = document.querySelector('.card');
+const details = document.querySelector('.details');
+const time = document.querySelector('img.time');
+const icon = document.querySelector('.icon img');
+const forcast = new Forcast();
+
+const updateUI = (data) => {
+
+    // const cityDets = data.cityDets;
+    // const weather = data.weather;
+
+    // desctructre props
+
+    const { cityDets, weather } = data;
+    console.log(weather)
+
+    // update details template
+    details.innerHTML = `
+        <h5 class="my-3">${cityDets.EnglishName}</h5>
+        <div class="my-3">${weather.WeatherText}</div>
+        <div class="display-4 my-4">
+        <span>${weather.Temperature.Metric.Value}</span>
+        <span>&deg;C</span>
+    `;
+
+// update the night/day & icons
+const iconSrc = `img/icons/${weather.WeatherIcon}.svg`;
+
+icon.setAttribute('src', iconSrc);
+
+
+let timeSrc = weather.IsDayTime ? 'img/day.svg' : 'img/night.svg';
+
+
+
+time.setAttribute('src', timeSrc);
+
+    // remove d-none if present
+
+    if(card.classList.contains('d-none')){
+        card.classList.remove('d-none')
+    }
+
+};
+
+
+
+cityForm.addEventListener('submit', e => {
+    // prevent default action
+    e.preventDefault();
+    
+    // get city value
+    const city = cityForm.city.value.trim();
+    cityForm.reset();
+
+    // update the ui with new city
+    forcast.updateCity(city)
+    .then(data =>  updateUI(data))
+    .catch(err => console.log(err));
+
+    // set local storage
+
+    localStorage.setItem('city', city);
+});
+
+if(localStorage.getItem('city')){
+    forcast.updateCity(localStorage.getItem('city'))
+        .then(data => updateUI(data))
+            .catch(err => console.log(err));
+}
